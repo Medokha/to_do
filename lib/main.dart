96816fd50a4a10495e85aa51b5929e53df8a49dd
Dell <Khaldalsydmhmd5@gmail.com>
2022-12-12 07:08:20 -0800

    connect to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/layout/home_layout.dart';
@@ -7,7 +8,9 @@ import 'package:todo/provider/provider_settings.dart';
 import 'package:todo/shared/styles/my_theme.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp( MultiProvider(
     providers: [
       ChangeNotifierProvider(
